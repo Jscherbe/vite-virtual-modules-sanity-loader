e8bad51ba00a0ba7b655f06066aef06d47d0d722
Joseph Scherben <jscherbe@gmail.com>
2025-11-25 10:15:52 -0500

    Generate types, setup ignores to publish

---
+++ types/index.d.ts
@@ -0,0 +1,51 @@
+/**
+ * @typedef {import('@ulu/sanity-loader').SanityLoader} SanityLoader
+ * @typedef {import('@sanity/client').SanityClient} SanityClient
+ * @typedef {import('@sanity/image-url/lib/types/builder').ImageUrlBuilder} ImageUrlBuilder
+ */
+/**
+ * @typedef {object} VirtualModulesLoader
+ * @property {SanityClient} client - The configured Sanity client instance.
+ * @property {(options: object) => () => { watch: string[], load: () => Promise<string> }} defineLoader - Defines a virtual module loader.
+ * @property {(source: object) => ImageUrlBuilder} imageUrl - The Sanity image URL builder instance.
+ * @property {object} utils - Utility functions from the core loader.
+ */
+/**
+ * Creates the main loader for Vite virtual modules.
+ * @param {SanityLoader} sanityLoader - A pre-configured instance from `@ulu/sanity-loader`.
+ * @param {object} [globalOptions] - Global configuration options for all loaders.
+ * @param {string[]|null} [globalOptions.watch] - Glob patterns for files to watch. Set to `null` to disable watching by default.
+ * @param {object} [globalOptions.watchOptions] - Default options for the file watcher (chokidar).
+ * @param {string[]} [globalOptions.watchEvents] - Default events that trigger a reload.
+ * @returns {VirtualModulesLoader} The virtual modules loader instance.
+ */
+export function createVirtualModulesLoader(sanityLoader: SanityLoader, globalOptions?: {
+    watch?: string[] | null;
+    watchOptions?: object;
+    watchEvents?: string[];
+}): VirtualModulesLoader;
+export type SanityLoader = import("@ulu/sanity-loader").SanityLoader;
+export type SanityClient = import("@sanity/client").SanityClient;
+export type ImageUrlBuilder = import("@sanity/image-url/lib/types/builder").ImageUrlBuilder;
+export type VirtualModulesLoader = {
+    /**
+     * - The configured Sanity client instance.
+     */
+    client: SanityClient;
+    /**
+     * - Defines a virtual module loader.
+     */
+    defineLoader: (options: object) => () => {
+        watch: string[];
+        load: () => Promise<string>;
+    };
+    /**
+     * - The Sanity image URL builder instance.
+     */
+    imageUrl: (source: object) => ImageUrlBuilder;
+    /**
+     * - Utility functions from the core loader.
+     */
+    utils: object;
+};
+//# sourceMappingURL=index.d.ts.map
---
+++ types/logger.d.ts
@@ -0,0 +1,6 @@
+export namespace log {
+    function log(...args: any[]): void;
+    function warn(...args: any[]): void;
+    function error(...args: any[]): void;
+}
+//# sourceMappingURL=logger.d.ts.map
